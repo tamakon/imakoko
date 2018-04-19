54c36b40f81b5d4a5c5a8fe64495b232b030674e
h-haraguchi <account.cgtdnpck@me.com>
2018-04-20 00:24:25 +0900

    #9 位置情報APIのリクエスト

--- functions/index.js
+++ functions/index.js
@@ -10,11 +10,23 @@ exports.locations = functions.https.onRequest(locationApi);
 
 function makeLocationApi(database) {
   const api = express();
-  api.get("/", (req, res) => res.send({"location": "TODO"}));
+  api.get("/", (req, res) => {
+    database.ref("locations").child(req.query.passcode).once("value").then(function (snapshot) {
+      const location = snapshot.val();
+      if (location !== null) {
+        res.json({"location": location});
+      } else {
+        res.status(404).end();
+      }
+    }).catch(function (error) {
+      console.error(error);
+      res.status(500).end();
+    });
+  });
   api.post("/", (req, res) => {
     const onetimeAccessNumber = zeroFill(4, randomNumber({min: 0, max: 9999, integer: true}));
     database.ref("locations").child(onetimeAccessNumber).set(req.body.location);
-    res.send({"passcode": onetimeAccessNumber});
+    res.json({"passcode": onetimeAccessNumber});
   });
   return api;
 }
