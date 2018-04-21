82218319abd9e1d8a38ab01a2f857977d7624181
h-haraguchi <account.cgtdnpck@me.com>
2018-04-21 14:48:10 +0900

    #13 位置情報取得のバリデーションを作成

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 const express = require("express");
-const { body, validationResult } = require('express-validator/check');
+const {body, query, validationResult} = require('express-validator/check');
 const randomNumber = require("random-number");
 const zeroFill = require("zero-fill");
 
@@ -11,18 +11,25 @@ exports.locations = functions.https.onRequest(locationApi);
 
 function makeLocationApi(database) {
   const api = express();
-  api.get("/", (req, res) => {
-    database.ref("locations").child(req.query.passcode).once("value").then(function (snapshot) {
-      const location = snapshot.val();
-      if (location !== null) {
-        res.json({"location": location});
-      } else {
-        res.status(404).end();
-      }
-    }).catch(function (error) {
-      console.error(error);
-      res.status(500).end();
-    });
+  api.get("/", [
+    query("passcode").custom((value) => /^[0-9]{4}$/.test(value))
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      database.ref("locations").child(req.query.passcode).once("value").then(function (snapshot) {
+        const location = snapshot.val();
+        if (location !== null) {
+          res.json({"location": location});
+        } else {
+          res.status(404).end();
+        }
+      }).catch(function (error) {
+        console.error(error);
+        res.status(500).end();
+      });
+    } else {
+      res.status(400).end();
+    }
   });
   api.post("/", [
     body("location.latitude", "").custom((value) => /^\(?[+-]?(90(\.0+)?|[1-8]?\d(\.\d+)?)$/.test(value)),
