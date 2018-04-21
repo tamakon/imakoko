0e55a1f3070c3bbf2115d4f1766c8a17a9cee1d1
h-haraguchi <account.cgtdnpck@me.com>
2018-04-21 14:44:16 +0900

    #13 位置情報送信のバリデーションを作成

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 const express = require("express");
+const { body, validationResult } = require('express-validator/check');
 const randomNumber = require("random-number");
 const zeroFill = require("zero-fill");
 
@@ -23,10 +24,19 @@ function makeLocationApi(database) {
       res.status(500).end();
     });
   });
-  api.post("/", (req, res) => {
-    const onetimeAccessNumber = zeroFill(4, randomNumber({min: 0, max: 9999, integer: true}));
-    database.ref("locations").child(onetimeAccessNumber).set(req.body.location);
-    res.json({"passcode": onetimeAccessNumber});
+  api.post("/", [
+    body("location.latitude", "").custom((value) => /^\(?[+-]?(90(\.0+)?|[1-8]?\d(\.\d+)?)$/.test(value)),
+    body("location.longitude").custom((value) => /^\s?[+-]?(180(\.0+)?|1[0-7]\d(\.\d+)?|\d{1,2}(\.\d+)?)\)?$/.test(value))
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      const onetimeAccessNumber = zeroFill(4, randomNumber({min: 0, max: 9999, integer: true}));
+      database.ref("locations").child(onetimeAccessNumber).set(req.body.location);
+      res.json({"passcode": onetimeAccessNumber});
+    } else {
+      console.log(errors.mapped());
+      res.status(400).end();
+    }
   });
   return api;
 }
